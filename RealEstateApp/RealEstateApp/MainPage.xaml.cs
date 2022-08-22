f3e62523b3a6b57e4fdc2fe40fdf3b6adb7eb758
Jens Anker Bruun de Neergaard <jens5561@elevcampus.dk>
2022-08-22 13:48:41 +0200

    Opgave 1.1

--- RealEstateApp/RealEstateApp/MainPage.xaml.cs
+++ RealEstateApp/RealEstateApp/MainPage.xaml.cs
@@ -5,23 +5,23 @@ using Xamarin.Forms.Xaml;
 namespace RealEstateApp
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
-    public partial class MainPage : MasterDetailPage
+    public partial class MainPage
     {
         public MainPage()
         {
             InitializeComponent();
 
-            menuPage.NavigationListView.ItemSelected += NavigationListView_ItemSelected;
+            //menuPage.NavigationListView.ItemSelected += NavigationListView_ItemSelected;
         }
 
-        private void NavigationListView_ItemSelected(object sender, SelectedItemChangedEventArgs e)
-        {
-            if (e.SelectedItem is Models.MenuItem item)
-            {
-                Detail = new NavigationPage((Page)Activator.CreateInstance(item.Target));
-                menuPage.NavigationListView.SelectedItem = null;
-                IsPresented = false;
-            }
-        }
+        //private void NavigationListView_ItemSelected(object sender, SelectedItemChangedEventArgs e)
+        //{
+        //    if (e.SelectedItem is Models.MenuItem item)
+        //    {
+        //        Detail = new NavigationPage((Page)Activator.CreateInstance(item.Target));
+        //        menuPage.NavigationListView.SelectedItem = null;
+        //        IsPresented = false;
+        //    }
+        //}
     }
 }
